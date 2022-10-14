4409914bc3026e9af4d3403dd3c1b6e4293d82c3
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2022-10-14 17:55:29 -0300

    Exercicios do curso de JS do CursoemVideo

---
+++ aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) {
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+} //condição simples
+console.log('Diriga sempre usando cinto de seguranca')
