e1547ab3ce71132f683d2ee20795253f01046bff
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2022-10-16 14:49:39 -0300

    aula13

---
+++ aula 13/ambiente.js
@@ -0,0 +1,13 @@
+// Estrutura de repetição com teste lógico no inicio
+let contador = 1
+while (contador <= 3) {
+    console.log(`Passo ${contador }`)
+    contador++
+}
+
+// Estrutura de repetição com teste lógico no final
+let contador2 = 1
+do {
+    console.log(`NovoPasso ${contador2}`)
+    contador2++
+}   while (contador2 <= 3)
