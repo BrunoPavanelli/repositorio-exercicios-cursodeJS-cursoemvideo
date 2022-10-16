f80722c28881fd8610fc91cedf35c794b273fb17
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2022-10-16 16:30:52 -0300

    exclui corpo da funcao

--- aula 14/exercicio 4/scriptfor.js
+++ aula 14/exercicio 4/scriptfor.js
@@ -1,11 +1,4 @@
 function contadora() {
-    let c = document.getElementById('start')
-    let fim = document.getElementById('end')
-    let passo = c + document.getElementById('step')
 
-    for (c; c <= fim; passo) {
-        document.getElementById('resultado')
-        resultado.innerHTML = `${c}`
-    }
 }
 
