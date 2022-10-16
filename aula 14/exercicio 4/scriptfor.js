3dc159abc98e3283177f3d1ddb4f2217ea652353
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2022-10-16 16:41:59 -0300

    tentativas 2

--- aula 14/exercicio 4/scriptfor.js
+++ aula 14/exercicio 4/scriptfor.js
@@ -1,4 +1,13 @@
 function contadora() {
+    let c = window.document.getElementById('start')
+    let fim = window.document.getElementById('end')
+    let passo = window.document.getElementById('step')
+    resultado.innerHTML = 'Contando ...'
+
+    do {
+        resultado.innerHTML = `${c}`
+        c + passo
+    }  while (c <= fim)
 
 }
 
